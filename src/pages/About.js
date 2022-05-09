4b71fc5056668bee027993bf3914f40fa3ee3775
Zidane Tedo <tedozidane@gmail.com>
2022-05-09 19:38:54 +0100

    Premiere version du projet WorldCountrySite avec la liste de tout les pays et leur drapeau , le nom des pays et la nombre de population avec un css basique

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Logo/>
+            <Navigation/>
+            <h2>A Propos</h2>
+            <p>  
+            </p>
+        </div>
+    );
+};
+
+export default About;
